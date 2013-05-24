b9d89238ae969c8c5f8e9d6a5275e6581b2d6167
Anastassiya  Aipova <aipova>
2013-05-24 12:48:19 +0700

    1

--- Compute/src/compute/RmiStarter.java
+++ Compute/src/compute/RmiStarter.java
@@ -13,19 +13,22 @@ import java.net.UnknownHostException;
  */
 public class RmiStarter {
 
-    public static void startRmi(Class<?> classToAddInCodebase) throws UnknownHostException {
+    public static void startRmi(Class<?> classToAddInCodebase) throws
+        UnknownHostException {
 
-        /* Use follow string for jdk 1.7-update 21 and newer
-         *  
+        /*
+         * Use follow string for jdk 1.7-update 21 and newer
+         *
          * System.setProperty("java.rmi.server.useCodebaseOnly", "false");
          */
-
+        System.setProperty("java.rmi.server.useCodebaseOnly", "false");
         if (classToAddInCodebase != null) {
-            System.setProperty("java.rmi.server.codebase", classToAddInCodebase
-                    .getProtectionDomain().getCodeSource().getLocation().toString());
+            System.setProperty("java.rmi.server.codebase",
+                classToAddInCodebase.getProtectionDomain().getCodeSource().getLocation().toString());
 
         }
-        System.setProperty("java.security.policy", PolicyFileLocator.getLocationOfPolicyFile());
+        System.setProperty("java.security.policy",
+            PolicyFileLocator.getLocationOfPolicyFile());
 
         if (System.getSecurityManager() == null) {
             System.setSecurityManager(new SecurityManager());
